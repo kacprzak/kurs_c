83c38c32dd685922dc7331ea89c826665524357c
Marcin Kacprzak <martin.kacprzak@gmail.com>
2012-11-15 00:55:19 +0100

    Cw 07-01

---
+++ cw07/cw07-01.c
@@ -0,0 +1,32 @@
+// cw07-01
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  if (argc < 2) {
+    puts("Nie podano argumentu!");
+    return -1;
+  }
+
+  int n = atoi(argv[1]);
+  printf("%d! = ", n);
+
+  if (n < 0) {
+    puts("Argument nie może być mniejszy od zera!");
+    return -2;
+  }
+
+  if (n == 0) {
+    puts("1");
+    return 0;
+  }
+
+  long long unsigned result = 1;
+  do {
+    result *= n--;
+  } while (n > 1);
+  
+  printf("%llu\n", result);
+  return 0;
+}
