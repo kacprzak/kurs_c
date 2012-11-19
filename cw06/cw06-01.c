e04ec5a8437663a47b7f97103c32ffdc53c15417
Marcin Kacprzak <martin.kacprzak@gmail.com>
2012-11-19 13:51:47 +0100

    Bardziej ogólne podejście do cw06-01

--- cw06/cw06-01.c
+++ cw06/cw06-01.c
@@ -1,5 +1,5 @@
 // cw06-01
-#include <stdio.h>
+#include <stdio.h> // printf, puts
 #include <math.h>  // sqrt
 
 int main(void)
@@ -9,13 +9,20 @@ int main(void)
   double b = 1;
   double c = -2;
 
-  double delta_sqr = sqrt(b*b - 4*a*c); // sqrt(17) = 4.12
-
-  double x1 = (-b - delta_sqr)/(2*a); // -1.28
-  double x2 = (-b + delta_sqr)/(2*a); //  0.78
-  
   printf("Dla równania kwadratowego o współczynnikach:\n a = %.2f, b = %.2f i c = %.2f\n", a, b, c);
-  printf("Obliczone pierwiastki to:\n x1 = %.2f, x2 = %.2f\n", x1, x2);
+  double delta = b*b - 4*a*c; // 17
+
+  if (delta > 0) {
+    double x1 = (-b - sqrt(delta))/(2*a); // -1.28
+    double x2 = (-b + sqrt(delta))/(2*a); //  0.78
+    printf("Obliczone pierwiastki to:\n x1 = %.2f, x2 = %.2f\n", x1, x2);
+  } else if (delta < 0) {
+    puts("Równanie nie posiada rozwiązań");
+  } else {
+    // Dla delty równej 0
+    double x = -b/(2*a);
+    printf("Równanie ma jedno rozwiązanie:\n x = %.2f\n", x);
+  }
 
   return 0;
 }
