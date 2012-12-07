e2b2f952340ed5ebcb8d52b5f516c8acd1eaff2d
Marcin Kacprzak <kacprzak@galaxy.agh.edu.pl>
2012-12-07 21:43:43 +0100

    cw11-03

---
+++ cw11/cw11-03.php
@@ -0,0 +1,31 @@
+<?php
+$liczba1 = $_GET['liczba1'];
+$liczba2 = $_GET['liczba2'];
+$dzialanie = $_GET['dzialanie'];
+?>
+<html>
+	<head>
+		<title>cw11-03 - Wynik</title>
+	</head>
+	<body>
+<?php
+echo $liczba1 . ' ' . $dzialanie . ' ' . $liczba2 . ' = ';
+switch($dzialanie)
+{
+case '+':
+	echo $liczba1 + $liczba2;
+	break;
+case '-':
+	echo $liczba1 - $liczba2;
+	break;
+case '*':
+	echo $liczba1 * $liczba2;
+	break;
+case '/':
+	echo $liczba1 / $liczba2;
+	break;
+}
+?>
+	</body>
+</html>
+
