ed612fb0023d71e2a0a6214ec962bf15e3143fa5
Marcin Kacprzak <martin.kacprzak@gmail.com>
2012-12-08 16:59:04 +0100

    Przeformatowanie

--- cw11/cw11-03.php
+++ cw11/cw11-03.php
@@ -4,28 +4,29 @@ $liczba2 = $_GET['liczba2'];
 $dzialanie = $_GET['dzialanie'];
 ?>
 <html>
-	<head>
-		<title>cw11-03 - Wynik</title>
-	</head>
-	<body>
+  <head>
+    <title>cw11-03 - Wynik</title>
+  </head>
+  <body>
 <?php
 echo $liczba1 . ' ' . $dzialanie . ' ' . $liczba2 . ' = ';
+
 switch($dzialanie)
 {
 case '+':
-	echo $liczba1 + $liczba2;
-	break;
+  echo $liczba1 + $liczba2;
+  break;
 case '-':
-	echo $liczba1 - $liczba2;
-	break;
+  echo $liczba1 - $liczba2;
+  break;
 case '*':
-	echo $liczba1 * $liczba2;
-	break;
+  echo $liczba1 * $liczba2;
+  break;
 case '/':
-	echo $liczba1 / $liczba2;
-	break;
+  echo $liczba1 / $liczba2;
+  break;
 }
 ?>
-	</body>
+  </body>
 </html>
 
